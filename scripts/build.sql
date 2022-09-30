48163f6789b7a475c2b4e7d8cf7168747f553bdc
mjanes71 <mjanes71@gmail.com>
2022-09-30 13:53:00 -0400

    cleaned up build.sql

--- scripts/build.sql
+++ scripts/build.sql
@@ -1,13 +1,17 @@
+create schema if not exists import;
 drop table if exists import.master_plan;
 create table import.master_plan(
     start_time_utc text,
-    duration text,
-	date text,
-	team text,
-	spass_type text,
-	target text,
-	request_name text,
-	library_definition text,
-	title text,
-	description text
-    );
+    durtion text,
+    date text,
+    team text,
+    spass_type text,
+    target text,
+    request_name text,
+    library_definition text,
+    title text,
+    description text
+);
+COPY import.master_plan
+FROM '/Users/meganracheljanes/Documents/myStuff/curious-moon/data/master_plan.csv'
+WITH DELIMITER ',' HEADER CSV;
